ec3f4aec05ef37ff595a37bec109d2f5a18561a4
Enegadi Ismail <ismail0enegadi@gmail.com>
2024-05-20 16:00:06 +0100

    login page

---
+++ pages/login/Login.tsx
@@ -0,0 +1,51 @@
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  TextInput,
+  Keyboard,
+    } from "react-native";
+import React, { useState } from "react";
+import styles from "./styles";
+import { Button } from "react-native-elements";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isloading, setIsLoading] = useState(false);
+
+  const onLoginPress = () => {
+    console.log("Login button pressed");
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+            <Text style={styles.logoText}>Casino Game</Text>
+            <TextInput
+              placeholder="Username"
+              style={styles.loginFormTextInput}
+              value={username}
+              onChangeText={setUsername}
+            />
+            <TextInput
+              placeholder="Password"
+              style={styles.loginFormTextInput}
+              secureTextEntry={true}
+              onChangeText={setPassword}
+              value={password}
+            />
+            <Button
+              buttonStyle={styles.loginButton}
+              onPress={() => onLoginPress()}
+              title="Login"
+            />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
